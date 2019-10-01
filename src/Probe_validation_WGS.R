aa77a6e38cac682114e5a6cf9851b9c738c71029
Ido Bar <ido.idobar@gmail.com>
2019-10-01 15:43:45 +1000

    Produced alignment plots of primer regions

--- src/Probe_validation_WGS.R
+++ src/Probe_validation_WGS.R
@@ -1,49 +1,66 @@
 devtools::source_gist("7f63547158ecdbacf31b54a58af0d1cc", filename = "util.R")
-BiocManager::install(c("GenomicRanges", "rtracklayer", "GenomicFeatures", "Rsamtools", 
-                       "Gviz"), update = FALSE)
-BiocManager::install("Gviz")
-library(Gviz)
-library(tidyverse)
-pacman::p_load(tidyverse)
+bioc_packs <- c("GenomicRanges", "rtracklayer", "GenomicFeatures", "Rsamtools", 
+                "Gviz")
+BiocManager::install(bioc_packs, update = FALSE)
+`BiocManager::install("Gviz")
+
+pacman::p_load(char = c("tidyverse","seqinr", bioc_packs ))
 
 
 #### Read data ####
-primer_table <- readxl::read_excel("./A.rabiei_primers/Primer_table.xlsx") %>% 
-  dplyr::filter(!is.na(NCBI_Accession)) %>% 
-  dplyr::rename(Gene_coords=`Gene genomic coords (Me14)`) %>% 
-  separate(Gene_coords, 
-           c("Chr", "Gene_start", "Gene_end"), sep = "([\\:-])", remove = FALSE) %>% 
-  separate(`Primer coords`, c("Primer_start", "Primer_end")) %>% 
-  mutate(Amplicon= sub("-[RF]$", "", Primer)) %>% 
-  mutate_at(vars(ends_with("_end")), as.numeric) %>% 
-  mutate_at(vars(ends_with("_start")), as.numeric)
+primer_table <- readxl::read_excel("./data/primer_info_marzia.xlsx") 
+primer_table %>% filter(Target_species=="B. cinerea") %>% group_by(NCBI_Accession) %>% dplyr::slice(1) %>% 
+  dplyr::select(Gene, NCBI_Accession) %>%   write_tsv("./data/primer_genes.txt")
+# export as fasta for BLAST
+util$write.fasta(primer_table[c("Primer", "Primer_sequence")], 
+            filename = "data/primer_seqs.fasta")
+# BLAST the file against the genome
+# read blast results
+blast_res <- read_tsv("data/primer_seqs.outfmt6", col_names = c("qseqid", "sseqid", "pident", "length",
+"mismatch", "gapopen", "qstart", "qend", "sstart", "send", "evalue", "bitscore")) 
 
-bam_files <- list.files("./alignment_files/" , pattern = ".bam$", full.names = TRUE)
 
-gff_file <- "Reference_genomes/Arab_me14.gff3"
+# 
+#   dplyr::filter(!is.na(NCBI_Accession)) %>% 
+#   dplyr::rename(Gene_coords=`Gene genomic coords (Me14)`) %>% 
+#   separate(Gene_coords, 
+#            c("Chr", "Gene_start", "Gene_end"), sep = "([\\:-])", remove = FALSE) %>% 
+#   separate(`Primer coords`, c("Primer_start", "Primer_end")) %>% 
+#   mutate(Amplicon= sub("[-/][RF]$", "", Primer)) %>% 
+#   mutate_at(vars(ends_with("_end")), as.numeric) %>% 
+#   mutate_at(vars(ends_with("_start")), as.numeric)
+
+wgs_folder <- "../AGRF_CAGRF20074_HFLHKDRXX/AGRF_BT2_process_19_09_2019"
+bam_files <- list.files(wgs_folder , pattern = ".bam$", full.names = TRUE)
+# bam_files %>% walk(~indexBam(.x))
+# indexBam(bam_files)
+
+gff_file <- "data/genome_assemblies/GCA_000143535.4_ASM14353v4_genomic.gff"
 options(ucscChromosomeNames=FALSE)
-# import genpome as fasta
-seqs <- Biostrings::readDNAStringSet("Reference_genomes/A.rabiei_me14.fasta")
+# import genome as fasta
+seqs <- Biostrings::readDNAStringSet("data/genome_assemblies/GCA_000143535.4_ASM14353v4_genomic.fna")
 
 # Import gff as GRanges
 # gff <- import.gff(gff_file)
 
 # Import gff as TxDb
-txdb <- GenomicFeatures::makeTxDbFromGFF(gff_file, format="gff3", dataSource = "CCDM Curtin",
-                        organism = "Ascochyta rabiei",
-                        taxonomyId = 5454)
+txdb <- GenomicFeatures::makeTxDbFromGFF(gff_file, format="gff3", dataSource = "GCA_000143535.4",
+                        organism = "Botrytis cinerea B05.10",
+                        taxonomyId = 332648)
 
-
-accessions <- unique(primer_table$NCBI_Accession)
+primer_blast <- primer_table %>% filter(grepl("cinerea", Target_species)) %>% 
+  left_join(blast_res %>% dplyr::select(Chr=sseqid, Primer=qseqid))
+accessions <- unique(primer_blast$NCBI_Accession)
 for (acc in accessions){
   # acc <- accessions[1]
-  reg <- primer_table %>% dplyr::filter(NCBI_Accession==acc)
+  reg <- primer_blast %>% dplyr::filter(NCBI_Accession==acc)
   loc <- unique(reg$Gene_coords)
   # st <- unique(reg$Gene_start) # + reg$Primer_start-5
   # en <- unique(reg$Gene_end) # + reg$Primer_end+5
   chr <- unique(reg$Chr)
+  chr_name <- names(seqs)[grepl(unique(reg$Chr), names(seqs))]
   #### Verify primer sequence ####
-  seq <- seqs[[chr]]
+  seq <- seqs[[chr_name]]
   
   primers_pos <- str_locate(toString(seq),  reg$Primer_sequence) %>% as_tibble() 
   missing_pos <- which(rowSums(is.na(primers_pos))>0)
@@ -58,7 +75,7 @@ for (acc in accessions){
   # Plot with Gviz
   # Add a sequence track
   strack <- SequenceTrack(
-    seqs, genome = "A. rabiei Me14",
+    seqs, genome = "Botrytis cinerea B05.10",
     chromosome = chr, from = st, to = en,
     cex=0.5, min.height =7, legend=TRUE
   )
@@ -73,22 +90,30 @@ for (acc in accessions){
   )
   # add alignment
   
-  reg_bam <- bam_files[grepl(glue::glue('{sub(":", "_", loc, fixed = TRUE)}'), bam_files)]
-  altrack <- AlignmentsTrack(
-    reg_bam, isPaired = TRUE,  showMismatches=TRUE, # col.mates = "deeppink",
+  reg_bam <- bam_files[grepl(glue::glue("cinerea.+{chr}"), bam_files)] 
+  altrack_Bc <- AlignmentsTrack(
+    bam_files[grepl(glue::glue("cinerea.+{chr}"), bam_files)], isPaired = TRUE,  
+    showMismatches=TRUE, name = "B. cinerea reads",# col.mates = "deeppink",
     # min.height = 12, max.height = 20, coverageHeight = 0.15, size = 50, 
     reverseStacking=FALSE, min.height = 2.5, # coverageHeight = 0.15,
     type=c("coverage", "pileup"), start = st, end = en
   )
   
+  altrack_Bf <- AlignmentsTrack(
+    bam_files[grepl(glue::glue("fabae.+{chr}"), bam_files)], isPaired = TRUE,  
+    showMismatches=TRUE, name = "B. fabae reads",# col.mates = "deeppink", 
+    # min.height = 12, max.height = 20, coverageHeight = 0.15, size = 50, 
+    reverseStacking=FALSE, min.height = 2.5, # coverageHeight = 0.15,
+    type=c("coverage", "pileup"), start = st, end = en
+  )
   # add highlighting 
-  ht1 <- HighlightTrack(trackList=list(altrack), 
+  ht1 <- HighlightTrack(trackList=list(altrack_Bc, altrack_Bf), 
                         start = c(primers_pos$start[1], primers_pos$start[2]),
                         end = c(primers_pos$end[1],primers_pos$end[2]), 
                         chromosome=chr)
-  LogMsg(glue::glue("Preparing plot for {reg$Amplicon[1]} on {chr}"))
+  LogMsg(glue::glue("Preparing plot for {reg$Gene[1]} on {chr}"))
   # plot 
-  pdf(glue::glue("plots/Gviz_alignment_{reg$Amplicon[1]}_region_{chr}_{st}-{en}.pdf"),
+  pdf(glue::glue("output/plots/Gviz_alignment_{reg$Gene[1]}_region_{chr}_{st}-{en}.pdf"),
       width = 8, height = 6)
   plotTracks(
     list(gtrack, strack, ht1, grtrack), # 
